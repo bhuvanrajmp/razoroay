eab7885b3163ce826fc495e5d54aeefa14a6b13e
bhuvanrajmp <bhuvanrajmp700@gmail.com>
2026-06-07 12:15:44 +0530

    Initial commit

---
+++ src/main/java/com/MyFirstSpringbootApp/razoroay/RazoroayApplication.java
@@ -0,0 +1,13 @@
+package com.MyFirstSpringbootApp.razoroay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazoroayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RazoroayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MyFirstSpringbootApp/razoroay/RazoroayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyFirstSpringbootApp.razoroay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazoroayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
